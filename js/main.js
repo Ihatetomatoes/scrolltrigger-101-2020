91e34fdaaa11d072992f083f505a3bb970fa8842
Petr Tichy <peter@ihatetomatoes.net>
2020-05-29 16:56:10 +1000

    simple tween with trigger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+gsap.registerPlugin(ScrollTrigger);
+
 function init(){
     
+    // Simple tween
+    gsap.to('#intro img', {opacity: 0, scrollTrigger: {
+        trigger: '#intro',
+        start: 'top top',
+        end: 'bottom center',
+        scrub: true,
+        markers: true
+    }});
+
 }
 
 window.addEventListener('load', function(){
