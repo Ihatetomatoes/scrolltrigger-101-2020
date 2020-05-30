c98474b44300b4f40680d562753c046d050d413a
Petr Tichy <peter@ihatetomatoes.net>
2020-05-30 12:52:18 +1000

    toggle class

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ gsap.registerPlugin(ScrollTrigger);
 function init(){
     
     // Simple tween
-    gsap.to('#intro img', {opacity: 0, scrollTrigger: {
-        trigger: '#intro',
-        start: 'top top',
-        end: 'bottom center',
-        scrub: true,
+    // gsap.to('#intro img', {opacity: 0, scrollTrigger: {
+    //     trigger: '#intro',
+    //     start: 'top top',
+    //     end: 'bottom center',
+    //     scrub: true,
+    //     markers: true
+    // }});
+
+    // toggle css class
+    gsap.set("#project02", {scrollTrigger: {
+        trigger: '#project02',
+        start: 'top bottom-=10%',
+        end: 'bottom center-=10%',
+        toggleClass: 'active',
         markers: true
     }});
 
