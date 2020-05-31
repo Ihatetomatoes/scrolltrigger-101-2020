4ee500c409ece8fad6db909829fae469e5f3f17a
Petr Tichy <peter@ihatetomatoes.net>
2020-05-31 10:32:27 +1000

    simple parallax effect

--- js/main.js
+++ js/main.js
@@ -12,13 +12,27 @@ function init(){
     // }});
 
     // toggle css class
-    gsap.set("#project02", {scrollTrigger: {
-        trigger: '#project02',
-        start: 'top bottom-=10%',
-        end: 'bottom center-=10%',
-        toggleClass: 'active',
-        markers: true
-    }});
+    // gsap.set("#project02", {scrollTrigger: {
+    //     trigger: '#project02',
+    //     start: 'top bottom-=10%',
+    //     end: 'bottom center-=10%',
+    //     toggleClass: 'active',
+    //     markers: true
+    // }});
+
+    // simple parallax
+    const parallaxTl = gsap.timeline({
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.bcg-parallax',
+            start: 'top bottom',
+            scrub: true
+        }
+    });
+
+    parallaxTl
+        .from('.content-wrapper', {duration: 0.4, autoAlpha: 0}, 0.4)
+        .from('.bcg', {duration: 2, y: '-30%'}, 0);
 
 }
 
